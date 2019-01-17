6a8c3cd9bb11bc3025ab766a449e1a9d58f9b3f6
liAbbott <826858022@qq.com>
2019-01-17 18:33:01 +0800

    修改安卓首页登录登出的错误

--- jsm_base/src/main/java/com/zst/ynh_base/net/BaseParams.java
+++ jsm_base/src/main/java/com/zst/ynh_base/net/BaseParams.java
@@ -1,6 +1,7 @@
 package com.zst.ynh_base.net;
 
 import android.util.ArrayMap;
+import android.util.Log;
 
 import com.blankj.utilcode.util.DeviceUtils;
 import com.zst.ynh_base.BaseApplication;
@@ -9,18 +10,16 @@ import com.zst.ynh_base.util.VersionUtil;
 import java.util.Map;
 
 public class BaseParams {
-    private static Map<String,String> map;
-    public static Map getBaseParams(){
-        if (map==null){
-            map=new ArrayMap<>();
-        }
+
+    public static Map getBaseParams() {
+        Map<String, String> map = new ArrayMap<>();
         map.clear();
-        map.put("clientType","android");
-        map.put("appVersion",VersionUtil.getLocalVersion(BaseApplication.getContext())+"");
-        map.put("deviceId",DeviceUtils.getAndroidID());
-        map.put("deviceName",DeviceUtils.getModel());
-        map.put("osVersion",DeviceUtils.getSDKVersionName());
-        map.put("appMarket","");//渠道名
+        map.put("clientType", "android");
+        map.put("appVersion", VersionUtil.getLocalVersion(BaseApplication.getContext()) + "");
+        map.put("deviceId", DeviceUtils.getAndroidID());
+        map.put("deviceName", DeviceUtils.getModel());
+        map.put("osVersion", DeviceUtils.getSDKVersionName());
+        map.put("appMarket", "jsm");//渠道名
         return map;
     }
 }
